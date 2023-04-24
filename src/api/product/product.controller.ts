c8a43735346259a261af47d2ced6ba0c87bda8b0
Oluwatimilehin Oreoluwa Idowu <oluwatimilehin.id@gmail.com>
2023-04-24 17:37:49 +0100

    Change

--- src/api/product/product.controller.ts
+++ src/api/product/product.controller.ts
@@ -1,19 +1,41 @@
-import { type Request, type Response, type NextFunction } from 'express';
-import { ProductService } from './product.service';
-import { type CreateProductInput } from './product.schema';
+// import { type Request, type Response, type NextFunction } from 'express';
+// import { ProductService } from './product.service';
+// import { type CreateProductInput } from './product.schema';
 
-const productService = new ProductService();
+// const productService = new ProductService();
 
-export class ProductController {
-  public createProduct = async (req: Request<{}, {}, CreateProductInput>, res: Response, next: NextFunction) => {
-    try {
-      const productData = req.body;
+// export class ProductController {
+//   public createProduct = async (req: Request<{}, {}, IProduct>, res: Response, next: NextFunction) => {
+//     try {
+//       const productData: IProduct = req.body;
+//       const allImages = req.files as Express.Multer.File[];
 
-      const product = await productService.createProduct(productData);
+//       productData.productImages = [];
 
-      return res.status(201).json({ success: true, data: product });
-    } catch (err: any) {
-      next(err);
-    }
-  };
-}
+//       for (const key of Object.keys(allImages)) {
+//         if (key === 'productImages') {
+//           // @ts-expect-error not really an error
+//           for (const file of allImages[key]) {
+//             const path: string = file.path;
+//             const images = await uploadToCloudinary(path, 'Product-Images');
+//             productData.productImages.push(images);
+//           }
+//         }
+
+//         if (key === 'productRackImage') {
+//           // @ts-expect-error not really an error
+//           for (const file of allImages[key]) {
+//             const path: string = file.path;
+//             const image = await uploadToCloudinary(path, 'Product-Rack-Images');
+//             productData.productRackImage = image;
+//           }
+//         }
+//       }
+//       const product = await this.productService.createProduct(productData);
+
+//       return res.status(201).json({ message: 'Product created successfully', success: true, data: product });
+//     } catch (err: any) {
+//       next(err);
+//     }
+//   };
+// }
--- src/api/product/product.service.ts
+++ src/api/product/product.service.ts
@@ -1,27 +1,27 @@
-import { AppError } from '../../utils';
-import CategoryModel from '../category/category.model';
-import StoreModel from '../store/store.model';
-import ProductModel, { type Product } from './product.model';
+// import { AppError } from '../../utils';
+// import CategoryModel from '../category/category.model';
+// import StoreModel from '../store/store.model';
+// import ProductModel, { type Product } from './product.model';
 
-export class ProductService {
-  public async createProduct(productData: Partial<Product>) {
-    const category = await CategoryModel.findOne({ _id: productData.categoryId });
-    const store = await StoreModel.findOne({ _id: productData.storeId });
+// export class ProductService {
+//   public async createProduct(productData: Partial<Product>) {
+//     const category = await CategoryModel.findOne({ _id: productData.categoryId });
+//     const store = await StoreModel.findOne({ _id: productData.storeId });
 
-    if (category === null) {
-      throw new AppError(404, 'Category does not exist');
-    }
+//     if (category === null) {
+//       throw new AppError(404, 'Category does not exist');
+//     }
 
-    if (store === null) {
-      throw new AppError(404, 'Store does not exist');
-    }
+//     if (store === null) {
+//       throw new AppError(404, 'Store does not exist');
+//     }
 
-    if (productData.productImages.length === 0) {
-      throw new AppError(400, 'Product Images are required');
-    }
+//     if (productData.productImages.length === 0) {
+//       throw new AppError(400, 'Product Images are required');
+//     }
 
-    return await (
-      await (await ProductModel.create(productData)).populate('categoryId', 'categoryName')
-    ).populate('storeId', 'businessName');
-  }
-}
+//     return await (
+//       await (await ProductModel.create(productData)).populate('categoryId', 'categoryName')
+//     ).populate('storeId', 'businessName');
+//   }
+// }
