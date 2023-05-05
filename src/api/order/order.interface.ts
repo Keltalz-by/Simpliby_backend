b16bc528bd1ae0606060f5f9acc6e65c6f643c24
Oluwatimilehin Oreoluwa Idowu <oluwatimilehin.id@gmail.com>
2023-05-05 18:36:35 +0100

    fix: Make some fixes in the order feature

--- src/api/order/order.interface.ts
+++ src/api/order/order.interface.ts
@@ -1,11 +1,13 @@
-import { type IOrderItem } from '../orderItem/orderItem.interface';
-import { type IUser } from '../user/user.interface';
+// /* The OrderRoute class defines the routes for handling orders in an Express application, including
+// creating, retrieving, and deleting orders. */
+// import { type IOrderItem } from '../orderItem/orderItem.interface';
+// import { type IUser } from '../user/user.interface';
 
-export interface IOrder {
-  owner?: IUser['_id'];
-  orderItems: [IOrderItem['_id']];
-  phone?: string;
-  deliveryAddress?: string;
-  country?: string;
-  totalPrice?: number;
-}
+// export interface IOrder {
+//   owner?: IUser['_id'];
+//   orderItems: [IOrderItem['_id']];
+//   phone?: string;
+//   deliveryAddress?: string;
+//   country?: string;
+//   totalPrice?: number;
+// }
--- src/api/order/order.service.ts
+++ src/api/order/order.service.ts
@@ -1,78 +1,78 @@
-import OrderModel, { type Order } from './order.model';
-import OrderItemModel from '../orderItem/orderItem.model';
-import { type IOrder } from './order.interface';
-import { AppError } from '../../utils';
-import ProductModel from '../product/product.model';
+// import OrderModel, { type Order } from './order.model';
+// import OrderItemModel from '../orderItem/orderItem.model';
+// import { type IOrder } from './order.interface';
+// import { AppError } from '../../utils';
+// import ProductModel from '../product/product.model';
 
-export class OrderService {
-  public async createOrder(orderData: IOrder) {
-    const orderItemIds = Promise.all(
-      orderData.orderItems.map(async (item) => {
-        const newOrderItem = await OrderItemModel.create(item);
-        return newOrderItem._id;
-      })
-    );
-    const allOrderItemIds = await orderItemIds;
+// export class OrderService {
+//   public async createOrder(orderData: IOrder) {
+//     const orderItemIds = Promise.all(
+//       orderData.orderItems.map(async (item) => {
+//         const newOrderItem = await OrderItemModel.create(item);
+//         return newOrderItem._id;
+//       })
+//     );
+//     const allOrderItemIds = await orderItemIds;
 
-    const totalPrices = await Promise.all(
-      allOrderItemIds.map(async (itemId) => {
-        const orderItem = await OrderItemModel.findOne({ _id: itemId }).populate('product', 'price');
+//     const totalPrices = await Promise.all(
+//       allOrderItemIds.map(async (itemId) => {
+//         const orderItem = await OrderItemModel.findOne({ _id: itemId }).populate('product', 'price');
 
-        if (orderItem !== null) {
-          const orderProduct = await ProductModel.findOne({ _id: orderItem?.product });
-          if (orderProduct !== null) {
-            const totalPrice = parseInt(orderProduct?.price) * orderItem.quantity;
-            return totalPrice;
-          }
-          throw new AppError(404, 'Product not found');
-        }
-        throw new AppError(404, 'Order item does not exist');
-      })
-    );
-    const totalPrice = totalPrices.reduce((x, y) => x + y, 0);
-    return await (
-      await OrderModel.create({ ...orderData, orderItems: allOrderItemIds, totalPrice })
-    ).populate('owner', 'name');
-  }
+//         if (orderItem !== null) {
+//           const orderProduct = await ProductModel.findOne({ _id: orderItem?.product });
+//           if (orderProduct !== null) {
+//             const totalPrice = parseInt(orderProduct?.price) * orderItem.quantity;
+//             return totalPrice;
+//           }
+//           throw new AppError(404, 'Product not found');
+//         }
+//         throw new AppError(404, 'Order item does not exist');
+//       })
+//     );
+//     const totalPrice = totalPrices.reduce((x, y) => x + y, 0);
+//     return await (
+//       await OrderModel.create({ ...orderData, orderItems: allOrderItemIds, totalPrice })
+//     ).populate('owner', 'name');
+//   }
 
-  public async getOrders(): Promise<Order[]> {
-    const orders = await OrderModel.find().populate('owner', 'name').sort('-createdAt');
-    return orders;
-  }
+//   public async getOrders(): Promise<Order[]> {
+//     const orders = await OrderModel.find().populate('owner', 'name').sort('-createdAt');
+//     return orders;
+//   }
 
-  public async getUserSingleOrder(option: object): Promise<Order> {
-    const order = await OrderModel.findOne(option)
-      .populate('owner', 'name')
-      .populate({
-        path: 'orderItems',
-        populate: {
-          select: 'productName',
-          path: 'product',
-          populate: { select: 'businessName', path: 'storeId' }
-        }
-      });
+//   public async getUserSingleOrder(option: object): Promise<Order> {
+//     const order = await OrderModel.findOne(option)
+//       .populate('owner', 'name')
+//       .populate({
+//         path: 'orderItems',
+//         populate: {
+//           select: 'productName',
+//           path: 'product',
+//           populate: { select: 'businessName', path: 'storeId' }
+//         }
+//       });
 
-    if (order === null) {
-      throw new AppError(404, 'Order not found');
-    }
-    return order;
-  }
+//     if (order === null) {
+//       throw new AppError(404, 'Order not found');
+//     }
+//     return order;
+//   }
 
-  public async getAllOrdersOfUser(option: object): Promise<Order[]> {
-    const orders = await OrderModel.find(option);
+//   public async getAllOrdersOfUser(option: object): Promise<Order[]> {
+//     const orders = await OrderModel.find(option);
 
-    return orders;
-  }
+//     return orders;
+//   }
 
-  public async deleteOrder(orderId: string) {
-    const order = await OrderModel.findOne({ _id: orderId });
+//   public async deleteOrder(orderId: string) {
+//     const order = await OrderModel.findOne({ _id: orderId });
 
-    if (order !== null) {
-      order.orderItems.map(async (item) => {
-        await OrderItemModel.findByIdAndRemove(item);
-      });
-      return await order?.deleteOne();
-    }
-    throw new AppError(404, 'Order not found');
-  }
-}
+//     if (order !== null) {
+//       order.orderItems.map(async (item) => {
+//         await OrderItemModel.findByIdAndRemove(item);
+//       });
+//       return await order?.deleteOne();
+//     }
+//     throw new AppError(404, 'Order not found');
+//   }
+// }
